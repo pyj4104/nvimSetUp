92d562d9ea88a53d15b5eb28990e32dbea003789
Win 11 WSL Ubuntu <pyj4104@hotmail.com>
2024-03-19 12:05:15 -0400

    feat(LSP): Setting Up Mason and LSPConfig

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		'williamboman/mason.nvim',
+		config = function()
+			require('mason').setup()
+		end,
+		keys = {
+			{
+				'<leader>cM',
+				'<CMD>Mason<CR>',
+				desc = 'Mason'
+			},
+		},
+	},
+	{
+		'williamboman/mason-lspconfig.nvim',
+		config = function()
+			require('mason-lspconfig').setup({
+				ensure_installed = {
+					"lua_ls",
+					"omnisharp",
+					"gopls",
+					"html",
+					"biome",
+					"texlab",
+					'marksman',
+					'jedi_language_server',
+					'sqlls',
+					'rust_analyzer'
+				}
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+	}
+}
